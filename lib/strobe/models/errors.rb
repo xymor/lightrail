d8cf5b2794102babec82a5ce6cb62d954549d4ce
José Valim <jose.valim@gmail.com>
2011-10-07 15:18:53 +0200

    Add Strobe::Models::Errors.

---
+++ lib/strobe/models/errors.rb
@@ -0,0 +1,20 @@
+require 'active_model/errors'
+
+module Strobe
+  module Models
+    module Errors
+      def errors
+        @errors ||= Strobe::Models::Errors::Errors.new(self)
+      end
+
+      class Errors < ::ActiveModel::Errors
+        # Only show one message per error key.
+        def as_json(options=nil)
+          hash = ActiveSupport::OrderedHash.new
+          @messages.each { |k, v| hash[k] = v[0] }
+          hash
+        end
+      end
+    end/
+  end
+end
